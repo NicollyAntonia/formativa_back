36e514a455530d74f1539741e47ec6506b757cb7
Nicolly Ant�nia de Oliveira Souza <47819795808@>
2025-05-21 14:55:25 -0300

    vaiii agora dorivas

--- educacional/educacional/urls.py
+++ educacional/educacional/urls.py
@@ -18,10 +18,27 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Minha API Educacional",
+      default_version='v1',
+      description="Documentação da API para o sistema de gestão educacional",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="seuemail@exemplo.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('ambiente.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
 ]
